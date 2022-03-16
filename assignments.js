2a410055e0e738f1659c3d43ca71d5981819eeb2
Niseme <nijolesemele@gmail.com>
2022-03-16 13:14:47 +0200

    lecture: values and variables

---
+++ assignments.js
@@ -0,0 +1,6 @@
+// LECTURE: Value and Variables
+let country = 'Lithuania';
+let continent = 'Europe';
+let population = '2.7 mln.';
+
+console.log(`My country is ${country}. It is in ${continent} and population is ${population}`)
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JS is fun');
+//if (js === 'amazing') alert('JS is fun');
 console.log(40 + 6 + 9);
 
+let firstName = 'Jonas';
+console.log(firstName)
