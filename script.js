958a5e63033d7fc4765f86ad69692d484b874370
Niseme <nijolesemele@gmail.com>
2022-03-16 16:33:10 +0200

    data types

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-let js = 'amazing';
+/*let js = 'amazing';
 //if (js === 'amazing') alert('JS is fun');
 console.log(40 + 6 + 9);
 
 let firstName = 'Jonas';
-console.log(firstName)
+console.log(firstName)*/
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptisfun = 'YES!';
+console.log(javascriptisfun);
+console.log(typeof javascriptisfun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
