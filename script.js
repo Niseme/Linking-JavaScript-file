662c7deaf70656f03c6fc8af90fceff369489a1d
Niseme <nijolesemele@gmail.com>
2022-03-18 13:36:10 +0200

    21.Truthy and Falsy Values

--- script.js
+++ script.js
@@ -151,3 +151,27 @@ console.log('23' - '10' - 7);
 let n = '1' + 1;
 n = n - 1;
 console.log(n);*/
+
+//21.Truthy and Falsy Values
+
+//5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend at all");
+} else {
+    console.log("You should get a job");
+}
+
+let height;
+if (height) {
+    console.log("height is defined");
+} else {
+    console.log("height is undefined");
+}
